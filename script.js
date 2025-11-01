85d5d4c61dd09cadecfee63cd92b8d30017428ae
Ardan <ramadhanardan69@gmail.com>
2025-11-01 21:25:43 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Ambil tombol berdasarkan ID
+const tombolSapa = document.getElementById('button');
+
+// Saat tombol diklik, tampilkan pesan
+tombolSapa.addEventListener('click', function() {
+    alert('Halo! Selamat datang di proyek saya 🚀');
+});
